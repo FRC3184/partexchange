1039a12daaf6a36af2efdfd1c041467d1d382b95
Nick Schatz <nickbrickmaster@gmail.com>
2017-08-01 23:17:50 -0500

    Make login form prettier
    Change to flexbox. Now centered and more responsive.

--- account/login.php
+++ account/login.php
@@ -11,12 +11,12 @@
     header("Location: /account/");
   }
   ?>
-  <form action="../lib/verify.php" method="post" style="width:50%;">
+  <form action="../lib/verify.php" method="post" id="login-form">
 
     <div class="form-group">
       <label for="inputEmail" class="col-lg-2 control-label">Team #</label>
       <div class="col-lg-10">
-        <input type="text" name="username" class="form-control" id="inputEmail" placeholder="Team #" autocomplete="off" style="cursor: auto; background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QsPDhss3LcOZQAAAU5JREFUOMvdkzFLA0EQhd/bO7iIYmklaCUopLAQA6KNaawt9BeIgnUwLHPJRchfEBR7CyGWgiDY2SlIQBT/gDaCoGDudiy8SLwkBiwz1c7y+GZ25i0wnFEqlSZFZKGdi8iiiOR7aU32QkR2c7ncPcljAARAkgckb8IwrGf1fg/oJ8lRAHkR2VDVmOQ8AKjqY1bMHgCGYXhFchnAg6omJGcBXEZRtNoXYK2dMsaMt1qtD9/3p40x5yS9tHICYF1Vn0mOxXH8Uq/Xb389wff9PQDbQRB0t/QNOiPZ1h4B2MoO0fxnYz8dOOcOVbWhqq8kJzzPa3RAXZIkawCenHMjJN/+GiIqlcoFgKKq3pEMAMwAuCa5VK1W3SAfbAIopum+cy5KzwXn3M5AI6XVYlVt1mq1U8/zTlS1CeC9j2+6o1wuz1lrVzpWXLDWTg3pz/0CQnd2Jos49xUAAAAASUVORK5CYII=); background-attachment: scroll; background-position: 100% 50%; background-repeat: no-repeat;">
+        <input type="text" name="username" class="form-control" id="inputEmail" placeholder="Team #" autocomplete="off" style="cursor: auto;">
       </div>
       <label for="inputPassword" class="col-lg-2 control-label">Password</label>
       <div class="col-lg-10">
