fdc2fd970386e28512ddef4d7da7f9d58c68f5de
Nick Schatz <nickbrickmaster@gmail.com>
2017-08-01 20:52:17 -0500

    Add custom error documents
    Added documents for 404 and 500. Error 500 will send an email to the standard recipients with some details.
    Closes #18

--- error/404.php
+++ error/404.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>404: Not Found</title>
+    <title>Page Not Found</title>
 
     <?php include("../lib/head.html"); ?>
 </head>
 <body>
     <?php include '../lib/navbar.php'; ?>
-  
+
       <div class="jumbotron">
-        <h1>I couldn't find that for you.</h1>
+        <h1>404 Not Found</h1>
         <p class="lead">If you got here from a link on the site, please <a href="http://www.blazerobotics.org/contact-us.html">contact us.</a></p>
       </div>
-    
+
     <?php include '../lib/foot.html'; ?>
 </body>
 </html>
---
+++ error/500.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Internal Server Error</title>
+
+    <?php include("../lib/head.html"); ?>
+</head>
+<body>
+    <?php include '../lib/navbar.php'; ?>
+
+      <div class="jumbotron">
+        <h1>500 Internal Server Error</h1>
+        <p class="lead">All errors are reported to the webmaster. You can also create a ticket on
+          <a href="https://github.com/FRC3184/partexchange/issues">GitHub.</a></p>
+      </div>
+
+
+
+    <?php include '../lib/foot.html'; ?>
+</body>
+</html>
+
+<?php
+//Do all this after the page is generated so that any errors in the email code still show a 500 page
+include '../lib/mail.php';
+$email = setupMail();
+$email->setFrom("frc.parts.exchange@gmail.com");
+foreach ($mailRecipients as $addr) {
+  $email->addAddress($addr);
+}
+$email->isHTML(true);
+
+$email->Subject = "Error on parts.blazerobotics.org";
+$email->Body = sprintf("URL: %s<br/>Query String: %s", $_SERVER['REDIRECT_URL'], $_SERVER['REDIRECT_QUERY_STRING']);
+
+$res = send($email);
+?>
